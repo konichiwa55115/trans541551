a91b7d85f768d62ca6e692d526996dffe3203dae
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-02-05 22:44:15 +0200

    Update handlers_messages.py

--- src/transcriberbot/handlers_messages.py
+++ src/transcriberbot/handlers_messages.py
@@ -13,7 +13,6 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 import audiotools
 import html
-import phototools
 import logging
 import traceback
 import time
@@ -273,73 +272,6 @@ def video_note(bot, update):
     TranscriberBot.get().voice_thread_pool.submit(
       process_media_voice, bot, update, vn, "video_note"
     )
-
-def process_media_photo(bot, update, photo, chat):
-  chat_id = get_chat_id(update)
-  message_id = get_message_id(update)
-  is_group = chat_id < 0
-
-  message = None
-
-  if chat["photos_enabled"] == 1:
-    message = bot.send_message(
-      chat_id=chat_id,
-      text=R.get_string_resource("photo_recognizing", chat["lang"]),
-      reply_to_message_id=message_id,
-      parse_mode="html",
-      is_group=is_group
-    ).result()
-
-  file_id = photo[-1].file_id
-  file_path = os.path.join(config.get_config_prop("app")["media_path"], file_id)
-  bot.get_file(file_id).download(file_path)
-
-  def process(message):
-    if chat["qr_enabled"] == 1:
-      qr = phototools.read_qr(file_path, chat["lang"])
-      if qr is not None:
-        if is_group:
-          qr = R.get_string_resource("qr_result", chat["lang"]) + "\n" + qr
-
-        if message is not None:
-          bot.edit_message_text(
-            text=qr,
-            chat_id=chat_id,
-            message_id=message.message_id,
-            parse_mode="html",
-            is_group=is_group
-          )
-          return
-        else:
-          message = bot.send_message(
-            chat_id=chat_id,
-            text=qr,
-            reply_to_message_id=message_id,
-            parse_mode="html",
-            is_group=is_group
-          ).result()
-
-    if chat["photos_enabled"] == 1:
-      text = phototools.image_ocr(file_path, chat["lang"])
-      if text is not None:
-        if is_group:
-          text = R.get_string_resource("ocr_result", chat["lang"]) + "\n" + html.escape(text)
-        bot.edit_message_text(
-          text=text,
-          chat_id=chat_id,
-          message_id=message.message_id,
-          parse_mode="html",
-          is_group=is_group
-        )
-        return
-
-      bot.edit_message_text(
-        text=R.get_string_resource("photo_no_text", chat["lang"]),
-        chat_id=chat_id,
-        message_id=message.message_id,
-        parse_mode="html",
-        is_group=is_group
-      )
 
   retry = True
   retry_num = 0
